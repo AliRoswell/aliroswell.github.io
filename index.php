04e0de29558dec8c8f82897565700157ef3af216
Ali Roswell <96587047+AliRoswell@users.noreply.github.com>
2022-12-16 18:22:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-header("Refresh: 3; url=https://aliroswell.com");
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-<title>Ali Roswell</title>
-</head>
-<body>
-
-  <h2>AliRoswell.com sayfasına yönlendiriliyorsunuz</h2>
-
-</body>
-</html>   
+<iframe src="https://aliroswell.com" title="Ali Roswelll Web"></iframe>
